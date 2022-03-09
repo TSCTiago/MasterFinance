8908bb774afb7cd91350df3be0ae274ad718e028
Tiago Silva <87677785+TSCTiago@users.noreply.github.com>
2022-03-09 15:28:37 -0300

    commit

--- sript.js
+++ sript.js
@@ -12,74 +12,139 @@ const Modal = {
 
 const transactions = [
     {
-    id: 1,
-    description: 'Luz',
-    amount: -500000,
-    date: '23/01/2022',
+        description: 'Luz',
+        amount: 500000,
+        date: '23/01/2022',
     },
     {
-    id: 2,
-    description: 'website',
-    amount: 500000,
-    date: '23/01/2022',
+        description: 'website',
+        amount: 50000,
+        date: '23/01/2022',
     },
     {
-    id: 3,
-    description: 'internet',
-    amount: -20000,
-    date: '23/01/2022',
+        description: 'internet',
+        amount: 2000.00,
+        date: '23/01/2022',
     },
 ]
 
 
 const Transaction = {
-    incomes() {
+    all: transactions,
+    add(transaction){
+        Transaction.all.push(transaction)
 
+        App.reload()
+    },
+    remove(index){
+        Transaction.all.splice(index, 1)
+
+        App.reload()
+    },
+
+    incomes() {
+        let income = 0;
+        Transaction.all.forEach(transaction => {
+            if(transaction.amount > 0){
+                income += transaction.amount;
+            }
+        })
+        return  income;
     },
 
     expenses() {
-
+        let expense = 0;
+        Transaction.all.forEach(transaction => {
+            if(transaction.amount < 0){
+                expense += transaction.amount;
+            }
+        })
+        return expense;
     },
 
     total() {
-
+        return Transaction.incomes() + Transaction.expenses();
     }
 }
 
 const DOM = {
     transactionsContainer: document.querySelector('#data-table tbody'),
 
-    addTransaction(transaction, index){
+    addTransaction(transaction, index) {
         const tr = document.createElement('tr')
         tr.innerHTML = DOM.innerHTMLTransaction(transaction)
 
         DOM.transactionsContainer.appendChild(tr)
     },
-
-    innerHTMLTransaction(transaction){
-        const CSSclasse = transaction.amount > 0 ? "income" : "expense"
+    innerHTMLTransaction(transaction) {
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
 
         const amount = Utils.formatCurrency(transaction.amount)
 
         const html = `
             <td class="description">${transaction.description}</td>
-            <td class="${CSSclasse}">${transaction.amount}</td>
+            <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
             <td>
                 <img src="assets/minus.svg" alt="remover transação">
             </td>
             `
-            return html
+        return html
+    },
+    
+    updateBalance(){
+        document
+            .getElementById('incomeDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.incomes())
+        document
+            .getElementById('expenseDisplay')
+            .innerHTML =  Utils.formatCurrency(Transaction.expenses())
+        document
+            .getElementById('totalDisplay')
+            .innerHTML =  Utils.formatCurrency(Transaction.total())
+    },
+
+    clearTransactions(){
+        DOM.transactionsContainer.innerHTML = ""
     }
+
 }
- const Utils = {
-     formatCurrency(value){
+const Utils = {
+    formatCurrency(value) {
         const signal = Number(value) < 0 ? "-" : ""
 
-        value = String(value).replace()
-     }
- }
+        value = String(value).replace(/\D/g, "")
 
-transactions.forEach(function(transaction){
-    DOM.addTransaction(transaction)
-})
+        value = Number(value) / 100
+
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        })
+
+        return signal + value
+    }
+}
+
+const form = {
+    
+}
+
+const App = {
+    init(){
+        Transaction.all.forEach(transaction => {
+            DOM.addTransaction(transaction)
+        })
+        
+        DOM.updateBalance()
+        
+        
+    },
+    reload(){
+        DOM.clearTransactions()
+        App.init()
+    },
+}
+
+App.init()
+
