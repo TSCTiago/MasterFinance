e03bd672e264b42f5cda67bfc9999e3e2039a66b
Tiago Silva <87677785+TSCTiago@users.noreply.github.com>
2022-03-09 08:54:56 -0300

    mudança

--- sript.js
+++ sript.js
@@ -1,28 +1,85 @@
 const Modal = {
-    open(){
+    open() {
         document.querySelector('.modal-overlay')
-        .classList.add('active')
+            .classList.add('active')
     },
-    close(){
+    close() {
         document.querySelector('.modal-overlay')
-        .classList.remove('active')
-        
+            .classList.remove('active')
+
     }
 }
 
-const transactions = [{},{},{}]
+const transactions = [
+    {
+    id: 1,
+    description: 'Luz',
+    amount: -500000,
+    date: '23/01/2022',
+    },
+    {
+    id: 2,
+    description: 'website',
+    amount: 500000,
+    date: '23/01/2022',
+    },
+    {
+    id: 3,
+    description: 'internet',
+    amount: -20000,
+    date: '23/01/2022',
+    },
+]
 
 
 const Transaction = {
-    incomes(){
+    incomes() {
 
     },
 
-    expenses(){
+    expenses() {
 
     },
 
-    total(){
+    total() {
 
     }
 }
+
+const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+
+    addTransaction(transaction, index){
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+        DOM.transactionsContainer.appendChild(tr)
+    },
+
+    innerHTMLTransaction(transaction){
+        const CSSclasse = transaction.amount > 0 ? "income" : "expense"
+
+        const amount = Utils.formatCurrency(transaction.amount)
+
+        const html = `
+            <td class="description">${transaction.description}</td>
+            <td class="${CSSclasse}">${transaction.amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td>
+                <img src="assets/minus.svg" alt="remover transação">
+            </td>
+            `
+            return html
+    }
+}
+ const Utils = {
+     formatCurrency(value){
+        const signal = Number(value) < 0 ? "-" : ""
+
+        value = String(value).replace()
+     }
+ }
+
+transactions.forEach(function(transaction){
+    DOM.addTransaction(transaction)
+})
